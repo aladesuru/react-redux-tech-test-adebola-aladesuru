7f2da10c0d5f624bcbfbb86db0f2b87b29034812
adebola aladesuru <boluxyz@yahoo.com>
2022-06-24 10:32:33 +0100

    render page not found when try to list messages of non existing member

--- src/components/UserMessages/index.js
+++ src/components/UserMessages/index.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import * as actions from '../../actions/actionCreators';
 import Avatar from '../Avatar';
 import UserMessageDetails from '../UserMessageDetails';
+import PageNotFound from '../PageNotFound';
 
 const UserMessages = ({ messages, listMessage }) => {
   useEffect(() => {
@@ -13,40 +14,44 @@ const UserMessages = ({ messages, listMessage }) => {
         alert('Loading messages failed' + err);
       });
     }
-  });
+  }, []);
 
   return (
     <div>
-      <h1 className="headline">{`List Of Member's Messages`}</h1>
-      <Link to="/" className="view-all">
-        Back to view All
-      </Link>
-      {messages.length !== 0 ? (
-        <Avatar
-          source={messages[0].avatar}
-          firstName={messages[0].firstName}
-          lastName={messages[0].lastName}
-          showName={true}
-        />
-      ) : null}
-      <ul className="list-container">
-        {messages.map((content) => {
-          return (
-            <UserMessageDetails
-              key={content.id}
-              firstName={content.firstName}
-              lastName={content.lastName}
-              avatar={content.avatar}
-              message={content.message}
-              email={content.email}
-              userId={content.userId}
-              date={content.date}
-              showAvatar={false}
-              showEmailAndName={false}
-            />
-          );
-        })}
-      </ul>
+      {messages.length === 0 ? (
+        <PageNotFound />
+      ) : (
+        <>
+          <h1 className="headline">{`List Of Member's Messages`}</h1>
+          <Link to="/" className="view-all">
+            Back to view All
+          </Link>
+          <Avatar
+            source={messages[0].avatar}
+            firstName={messages[0].firstName}
+            lastName={messages[0].lastName}
+            showName={true}
+          />
+          <ul className="list-container">
+            {messages.map((content) => {
+              return (
+                <UserMessageDetails
+                  key={content.id}
+                  firstName={content.firstName}
+                  lastName={content.lastName}
+                  avatar={content.avatar}
+                  message={content.message}
+                  email={content.email}
+                  userId={content.userId}
+                  date={content.date}
+                  showAvatar={false}
+                  showEmailAndName={false}
+                />
+              );
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
