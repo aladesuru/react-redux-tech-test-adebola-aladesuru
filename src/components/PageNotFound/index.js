dc9ad3f4ed5aa293824a0cbe91b771270104312e
adebola aladesuru <boluxyz@yahoo.com>
2022-06-24 12:00:07 +0100

    add css for consistence link style

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div className="pageNotfound">
-      <p> Sorry page not found ! </p>
+    <div>
+      <p className="pageNotfound"> Sorry page not found ! </p>
       <Link to="/" className="view-all">
         Back to view all
       </Link>
